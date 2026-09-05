826c8ec420650cbb833584e05663a65407735b89
TDizzleFoShizzle <tommywilliams27@sky.com>
2026-09-05 21:54:15 +0100

    kind of improved portals

--- Stay in touch/Assets/Scripts/Level objects/PortalScript.cs
+++ Stay in touch/Assets/Scripts/Level objects/PortalScript.cs
@@ -1,46 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PortalScript : MonoBehaviour
 {
     [Header("Tuneable parameters")]
-    [SerializeField] PortalScript otherPortal;
+    [SerializeField] PortalScript otherPortal;  //which portal is this one linked to
 
     [Header("Runtime")]
-    bool active = true;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private List<GameObject> objectsInside = new List<GameObject>();    //which objects have been teleported into the portal and have not yet exited
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //TODO: do some shit with scale so that can make portals using scale instead of rotation to avoid some jank
 
-        if (active)
+        if (!objectsInside.Contains(collision.gameObject))
         {
+            otherPortal.objectsInside.Add(collision.gameObject);
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
             Vector3 posDiff = collision.transform.position - transform.position;
 
-            //rotate position and velcoity based on angle
-            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-            Vector3 newPosDiff =  Quaternion.Euler(otherPortal.transform.rotation.eulerAngles - transform.rotation.eulerAngles) * posDiff;
+            //rotate position based on angle and scale
+            Vector3 newPosDiff = Quaternion.Euler(otherPortal.transform.rotation.eulerAngles - transform.rotation.eulerAngles) * posDiff;
+            newPosDiff.x *= otherPortal.transform.localScale.x / transform.localScale.x;
+            rb.position = (otherPortal.transform.position + newPosDiff);
 
-            rb.MovePosition(otherPortal.transform.position + newPosDiff);
+            //rotate velocity based on angle and scale
             rb.linearVelocity = Quaternion.Euler(transform.rotation.eulerAngles) * rb.linearVelocity;
-
-            otherPortal.active = false;
+            rb.linearVelocityX *= otherPortal.transform.localScale.x / transform.localScale.x;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        active = true;
+        objectsInside.Remove(collision.gameObject);
     }
 }
