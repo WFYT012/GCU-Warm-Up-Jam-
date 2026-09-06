39dbc66cbbfe497a8fd65c85bd13450fc679f4ff
Connor Anderson <161883351+WFYT012@users.noreply.github.com>
2026-09-06 18:51:20 +0100

    main menu changes

---
+++ Stay in touch/Assets/Scripts/CharacterLauncherScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CharacterLauncherScript : MonoBehaviour
+{
+    public GameObject launchable;
+    public float timer = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 5)
+        {
+            timer = 0;
+            GameObject obj = Instantiate(launchable, transform.position, Quaternion.identity);
+            Rigidbody2D objRb = launchable.GetComponent<Rigidbody2D>();
+            objRb.AddForce(Vector2.right * 100);
+        }
+    }
+
+}
--- Stay in touch/Assets/Scripts/MenuButtonsScript.cs
+++ Stay in touch/Assets/Scripts/MenuButtonsScript.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System.Collections;
 
 public class MenuButtonsScript : MonoBehaviour
 {
+    public GameObject playButton;
+    public GameObject creditsButton;
+    public GameObject quitButton;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(ButtonIntro());
     }
 
     // Update is called once per frame
@@ -13,4 +20,26 @@ public class MenuButtonsScript : MonoBehaviour
     {
         
     }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Level1");
+        
+    }
+    public void StopButton()
+    {
+        Application.Quit();
+    }
+    public void CreditsButton()
+    {
+        // add credits
+    }
+    IEnumerator ButtonIntro()
+    {
+        yield return new WaitForSeconds(1f);
+        playButton.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, -89), 1.5f).SetEase(Ease.OutBounce);
+        creditsButton.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, -232), 1.5f).SetEase(Ease.OutBounce);
+        quitButton.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, -377), 1.5f).SetEase(Ease.OutBounce);
+    }
+
 }
