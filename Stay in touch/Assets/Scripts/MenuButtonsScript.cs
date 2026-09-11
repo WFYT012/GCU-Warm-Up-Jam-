e0f89026816cdf051d55225fd6ca049359cb9498
Connor Anderson <161883351+WFYT012@users.noreply.github.com>
2026-09-11 14:47:42 +0100

    CREDITS

--- Stay in touch/Assets/Scripts/MenuButtonsScript.cs
+++ Stay in touch/Assets/Scripts/MenuButtonsScript.cs
@@ -2,19 +2,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
 using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuButtonsScript : MonoBehaviour
 {
     public GameObject playButton;
     public GameObject creditsButton;
     public GameObject quitButton;
+    public Text credits;
 
     public DeathTransition dt;
+
+    [SerializeField] AudioResource menuSound;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(ButtonIntro());
+        credits.enabled = false;
     }
 
     // Update is called once per frame
@@ -25,15 +31,20 @@ public class MenuButtonsScript : MonoBehaviour
 
     public void PlayButton()
     {
+        SoundManagerScript.instance.PlaySoundClip(menuSound);
         dt.Death();
         MusicManagerScript.instance.FadeTrack();
     }
     public void StopButton()
     {
+        SoundManagerScript.instance.PlaySoundClip(menuSound);
         Application.Quit();
+
     }
     public void CreditsButton()
     {
+        SoundManagerScript.instance.PlaySoundClip(menuSound);
+        StartCoroutine(FlashCredits());
         // add credits
     }
     IEnumerator ButtonIntro()
@@ -43,5 +54,10 @@ public class MenuButtonsScript : MonoBehaviour
         creditsButton.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, -232), 1.5f).SetEase(Ease.OutBounce);
         quitButton.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, -377), 1.5f).SetEase(Ease.OutBounce);
     }
-
+    IEnumerator FlashCredits()
+    {
+        credits.enabled = true;
+        yield return new WaitForSeconds(1.5f);
+        credits.enabled = false;
+    }
 }
